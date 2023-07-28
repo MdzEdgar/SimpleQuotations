661e602fd76d9f3b4399ffcc31faf24b727fb93f
Edgar Méndez <mdzredgar@gmail.com>
2023-07-28 11:33:39 -0500

    Constructor creado

---
+++ simplequotations.php
@@ -0,0 +1,29 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class SimpleQuotations extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'simplequotations';
+        $this->tab = 'others';
+        $this->Version = '1.0.0';
+        $this->author = 'Edgar Mdz';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.7',
+            'max' => '1.7.9',
+        ];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Simple Quotations');
+        $this->description = $this->l('Create quotes from shopping carts.');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+        
+    }
+}
