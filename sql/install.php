893ea1d7c914171c9d385467c1fd78c71b797a50
Edgar Méndez <mdzredgar@gmail.com>
2023-07-28 13:28:31 -0500

    Correción de script SQL

--- sql/install.php
+++ sql/install.php
@@ -1,8 +1,8 @@
 <?php
 $sql = array();
 
-$sql[] = 'CREATE TABLE IF NO EXISTS `' ._DB_PREFIX_ . 'quotes` (
-	`id` int(11) auto_increment NOT NULL,
+$sql[] = 'CREATE TABLE IF NOT EXISTS `' ._DB_PREFIX_ . 'quotes` (
+	`id` int(11) AUTO_INCREMENT NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8mb4;';
 
