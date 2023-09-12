8b48d59ef4ce6ce3fb62be09f4c1c3165c42df09
Edgar Méndez <mdzredgar@gmail.com>
2023-09-12 14:57:03 -0500

    Modificaciones:

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -6,18 +6,37 @@ class SimpleQuotationsValidationModuleFrontController extends ModuleFrontControl
     {
         $cart = $this->context->cart;
         if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
-            Tools::redirect('index.php?controller=order&step=1');
+            Tools::redirect($this->context->link->getPageLink(
+                'order',
+                true,
+                (int) $this->context->language->id,
+                ['step' => 1,]
+            ));
 
-        $customer = new Customer((int)$cart->id_customer);
+        $customer = new Customer($cart->id_customer);
 
         if (!Validate::isLoadedObject($customer)) {
-            Tools::redirect('index.php?controller=order&step=1');
+            Tools::redirect($this->context->link->getPageLink(
+                'order',
+                true,
+                (int) $this->context->language->id,
+                ['step' => 1,]
+            ));
         }
 
-        $total_cart = $cart->getOrderTotal(false, Cart::BOTH_WITHOUT_SHIPPING, null, null, true);
+        $total_cart = (float) $cart->getOrderTotal(false, Cart::BOTH_WITHOUT_SHIPPING, null, null, true);
         if ($total_cart <=0)
         {
-            Tools::redirect('index.php?controller=order&step=1');
+            Tools::redirect($this->context->link->getPageLink(
+                'order',
+                true,
+                $this->context->link->getPageLink(
+                    'oder',
+                    true,
+                    (int) $this->context->language->id,
+                    ['step' => 1,]
+                )
+            ));
         }
 
         $cartdetails = $cart->getSummaryDetails();
